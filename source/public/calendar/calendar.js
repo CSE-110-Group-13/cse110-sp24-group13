27cbd7b7cd33547a5bc31fc4f9f3a0509eab59b5
hwaengfan <21hoangphan@gmail.com>
2024-05-14 12:58:20 -0700

    revert express server

--- source/public/calendar/calendar.js
+++ source/public/calendar/calendar.js
@@ -3,16 +3,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const button = document.querySelector('button');
   button.addEventListener('click', () => {
     // Calls the async function and should alert Hello World!
-    makeApiRequest().then(data => alert(data));
+    // makeApiRequest().then(data => alert(data));
+    alert("This is the calendar page!");
   });
 });
 
 // Make an API request
-async function makeApiRequest() {
-  const response = await fetch(window.location.origin.toString() + '/api/helloWorld', {
-    method: 'GET'
-  })
+// async function makeApiRequest() {
+//   const response = await fetch(window.location.origin.toString() + '/api/helloWorld', {
+//     method: 'GET'
+//   })
 
-  const data = await response.text();
-  return data;
-}
+//   const data = await response.text();
+//   return data;
+// }
