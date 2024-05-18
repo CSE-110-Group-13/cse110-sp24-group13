910fbc9cb3e706c7db737b51501b45e3d07f64ed
sarahpaulll <spaull@ucsd.edu>
2024-05-18 13:04:41 -0700

    update svgs and styling

--- source/addNewbutton/addNew.js
+++ source/addNewbutton/addNew.js
@@ -15,10 +15,11 @@ class AddNewBtn extends HTMLElement {
         background-color: #F8F8F8;
         position: fixed;
         top: 5%;
-        left: 102%;
+        left: 100%; 
         justify-content: center;
         align-items: center;
-        transition: transform 0.6s ease-in-out;
+        border-radius: 15px;
+        transition: opacity 2s ease-in-out;
         transform: translateX(0%);
       }
 
@@ -37,6 +38,7 @@ class AddNewBtn extends HTMLElement {
         font-family: 'Varela Round';
         width: 5em;
         align-items: center;
+        padding-left: 5px;
       }
 
       a {
@@ -47,6 +49,7 @@ class AddNewBtn extends HTMLElement {
         gap: 0.3em;
         font-family: 'Varela Round', sans-serif;
         padding: 7px;
+        padding-left: 20px;
         
       }
 
@@ -58,16 +61,33 @@ class AddNewBtn extends HTMLElement {
         position: absolute;
         top: 4%;
         left: 90%;
+        display: flex;
+        flex-direction: column;
+        gap: 0.3em;
         align-items: center;
         justify-content: center;
-        background: none;
+        background: #F8F8F8;
+        height: 10vh;
+        width: 8%;
+        border-radius: 20px;
         border: none;
-        transition: ease;
-        font-family: 'Varela Round', sans-serif;
+        transition: opacity 2s ease-in-out;
+        
+      }
+
+      button span {
+        color: #000;
+        font-size: 1em;
+        font-family: 'Varela Round';
+        width: 5em;
+        align-items: center;
 
       }
 
-
+      button svg {
+        width: 3em;
+        height: auto;
+      }
     `
 
     // Attach element to shadow dom
@@ -99,9 +119,9 @@ class AddNewBtn extends HTMLElement {
 
     // Create add new button
     const addBtn = document.createElement('button');
-    addBtn.textContent = "Add New";
     addBtn.id = "add-button";
-    addBtn.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>';
+    addBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>';
+    addBtn.innerHTML += "<span> Add New <\span>";
 
     // Add event listener to the add new button
     addBtn.addEventListener("click", () => {
@@ -109,23 +129,27 @@ class AddNewBtn extends HTMLElement {
       
       if (addNewBtnContainer.classList.contains("open")) {
         addBtn.innerHTML = '';
-        addBtn.textContent = '>';
-        addBtn.style.left = '81.5%';
-        addBtn.style.top = '8%';
+        addBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>';
+        addBtn.style.left = '77%';
+        addBtn.style.top = '5%';
+        addBtn.querySelector('svg').style.width = '1em';
+        addBtn.style.background = 'none';
       }
       else{
         addBtn.innerHTML = '';
-        addBtn.textContent = 'Add New';
-        addBtn.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>';
+        addBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>';
+        addBtn.innerHTML += "<span> Add New <\span>";
         addBtn.style.top = '4%';
         addBtn.style.left = '90%';
+        addBtn.style.background = '#F8F8F8';
+        addBtn.style.height = '10vh';
+        addBtn.style.width = '8%';
       }
     });
 
     // Append anchors and button to navbar
     navbar.appendChild(anchorToAddNewNote);
     navbar.appendChild(anchorToAddNewProject);
-
     
     addNewBtnContainer.appendChild(navbar);
     shadow.appendChild(addNewBtnContainer);
