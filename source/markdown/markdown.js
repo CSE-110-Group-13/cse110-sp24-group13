f8862c6e64b656adb00bc3612650f7b13a011f87
Maxim Podgore <maximjulianpodgore@Gmail.com>
2024-05-21 16:14:14 -0700

    Got the component working
    added a save and a load

--- source/markdown/markdown.js
+++ source/markdown/markdown.js
@@ -2,14 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
   const wysimark = createWysimark(document.getElementById("editor"), {
     initialMarkdown: "# Hello World",
     onChange: (markdown) => {
-      console.log(markdown)
-      window.localStorage.setItem("markdown", markdown);
+      //console.log(markdown)
+      window.localStorage.setItem("markdown", markdown.toString());
     },
   });
   
-  const savedMarkdownContent = window.localStorage.getItem("markdown");
-  if (savedMarkdownContent) {
-    console.log(savedMarkdownContent)
+  
+  const saveButton = document.getElementById("saveButton");
+  saveButton.addEventListener("click", function() {
+      window.localStorage.setItem("markdown", markdown.toString());
+    });
+  
+  const loadButton = document.getElementById("loadButton");
+  loadButton.addEventListener("click", function() {
+    const savedMarkdownContent = window.localStorage.getItem("markdown");
     wysimark.setMarkdown(savedMarkdownContent);
-  }
+    console.log(savedMarkdownContent);
+  });
 });
