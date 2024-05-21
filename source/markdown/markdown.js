a3557fb58a1ca012d80fec08e2597881215f86c5
salnav <sal.navroz@gmail.com>
2024-05-21 15:34:00 -0700

    Imported wysimark directly from the source (using unpkg) Added code for saving and loading markdown with local storage setMarkdown does not work

--- source/markdown/markdown.js
+++ source/markdown/markdown.js
@@ -1,20 +1,15 @@
-import { createWysimark } from "@wysimark/standalone"
-
-// For event listeners
-document.addEventListener("DOMContentLoaded", () => {
-  const display = document.getElementById('display-markdown');
-  const form = document.getElementById('markdownForm');
-  const input = document.getElementById('markdownInput');
-  
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const value = input.value;
-    const html = marked.parse(value);
-    display.innerHTML = `${html}<br/>`;  
+document.addEventListener("DOMContentLoaded", function() {
+  const wysimark = createWysimark(document.getElementById("editor"), {
+    initialMarkdown: "# Hello World",
+    onChange: (markdown) => {
+      console.log(markdown)
+      window.localStorage.setItem("markdown", markdown);
+    },
   });
+  
+  const savedMarkdownContent = window.localStorage.getItem("markdown");
+  if (savedMarkdownContent) {
+    console.log(savedMarkdownContent)
+    wysimark.setMarkdown(savedMarkdownContent);
+  }
 });
-
-// Assumes the existence of an HTML element with id="editor"
-const wysimark = createWysimark(document.getElementById("editor"), {
-  initialMarkdown: "# Hello World",
-})
