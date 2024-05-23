ec82d844e9cc5a274543b000fe10257845c33c9e
rhankin214 <100500846+rhankin214@users.noreply.github.com>
2024-05-23 13:14:55 -0700

    test notes display. Todo: sort function doesn't seem to be working. Notes aren't formatted yet.

--- source/library/library.js
+++ source/library/library.js
@@ -1,25 +1,27 @@
-/**
- * A function to load the entries into the container.
- * @param {*} entry 
- */
 import { 
     getNoteFromTable,
   } from "./NoteTable.js";
 
+/**
+ * Create html note elements from information in local stoarge
+ * @param {*} entry an entry of the note table in local storage
+ * @returns the created element
+ */
 function loadEntry(entry) {
     // create a new div element
-    const newNote = document.createElement("note");
-    
-    newNote.setAttribute("ID", entry["noteID"]);
+    const newNote = document.createElement("div");
+    newNote.setAttribute("class", "note");
+    newNote.setAttribute("noteID", entry["noteID"]);
     newNote.setAttribute("text", entry["text"]);
     newNote.setAttribute("date", entry["date"]);
-    newNote.setAttribute("lastEdited", entry["lastEditeds"]);
+    newNote.setAttribute("lastEdited", entry["lastEdited"]);
     newNote.setAttribute("title", entry["title"]);
     newNote.setAttribute("projectList", entry["projectList"]);
     newNote.setAttribute("favorited", entry["favorited"]);
+    //elements that must be displayed: title, date, lastEdited, tags, project
+    //Everything else can be attributes
+    newNote.innerText = entry['title'] + entry['date'];
 
-
-    // add the text node to the newly created div
     return newNote;
 }
 
@@ -49,31 +51,59 @@ function init() {
     let loadedNotes = [];
 
     //get and store all notes in an array
-    for(let i = 0; i < IDContainer.length; i++)
+    /*for(let i = 0; i < IDContainer.length; i++)
     {
         loadedNotes.push(loadEntry(getNoteFromTable(IDContainer[i])));
-    }
+    }*/
+
+    //Adding my own notes for testing. Remove later.
+    const testNote1 = {
+        "noteID" : "ABC",
+        "text" : "buy eggs",
+        "date" : "2024-5-21",
+        "lastEdited" : "2024-5-23",
+        "title" : "to do at grocery store",
+        "projectList" : [],
+        "favorited" : false,
+        "tags" : []
+    };
+
+    const testNote2 = {
+        "noteID" : "21kfasde",
+        "text" : "mow lawn",
+        "date" : "2024-5-22",
+        "lastEdited" : "2024-5-22",
+        "title" : "chores",
+        "projectList" : [],
+        "favorited" : false,
+        "tags" : []
+    };
+    loadedNotes.push(loadEntry(testNote1));
+    loadedNotes.push(loadEntry(testNote2));
 
     //init sort functions
-    const sortFunctions = newMap();
+    const sortFunctions = new Map();
 
     sortFunctions.set('title', (noteA, noteB) => {
-        if(noteA['title'] > noteB['title'])
+        const titleA = noteA.getAttribute('title'),
+        titleB = noteB.getAttribute('title');
+
+        if(titleA > titleB)
             return 1;
-        else if(noteA['title'] < noteB['title'])
+        else if(titleA < titleB)
             return -1;
         return 0;
     });
 
     sortFunctions.set('date', (noteA, noteB) => {
-        let da = new Date(noteA['date']),
-            db = new Date(noteB['date']);
+        let da = new Date(noteA.getAttribute('date')),
+            db = new Date(noteB.getAttribute('date'));
         return da - db;
     });
 
     sortFunctions.set('lastEdited', (noteA, noteB) => {
-        let da = new Date(noteA['lastEdited']),
-            db = new Date(noteB['lastEdited']);
+        let da = new Date(noteA.getAttribute('lastEdited')),
+            db = new Date(noteB.getAttribute('lastEdited'));
         return da - db;
     })
     //sort notes by title to start
@@ -92,12 +122,14 @@ function init() {
         while(children[0] != endBlock)
             entriesHolder.removeChild(children[0]);
 
-        //put all the notes in
+        //put all the notes in the html
         for(let i = 0; i < loadedNotes.length; i++)
         {
             entriesHolder.insertBefore(loadedNotes[i], endBlock);
         }
     }
+    displayNotes();
+
     sortBox.addEventListener("input", function sortAndDisplay(event)
     {
         loadedNotes.sort(sortFunctions[event.target.value]);
