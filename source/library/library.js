48cadd9fb9ea71f75432a8be99fcbb660e210d41
rhankin214 <100500846+rhankin214@users.noreply.github.com>
2024-05-23 12:05:42 -0700

    reworked existing code around recent backend work. TODO: properly display and style notes.

--- source/library/library.js
+++ source/library/library.js
@@ -2,88 +2,105 @@
  * A function to load the entries into the container.
  * @param {*} entry 
  */
-function loadEntries(entry) {
+import { 
+    getNoteFromTable,
+  } from "./NoteTable.js";
 
+function loadEntry(entry) {
     // create a new div element
-    const newDiv = document.createElement("div");
+    const newNote = document.createElement("note");
+    
+    newNote.setAttribute("ID", entry["noteID"]);
+    newNote.setAttribute("text", entry["text"]);
+    newNote.setAttribute("date", entry["date"]);
+    newNote.setAttribute("lastEdited", entry["lastEditeds"]);
+    newNote.setAttribute("title", entry["title"]);
+    newNote.setAttribute("projectList", entry["projectList"]);
+    newNote.setAttribute("favorited", entry["favorited"]);
 
-    // and give it some content
-    const textContent = document.createTextNode(entry);
 
     // add the text node to the newly created div
-    newDiv.appendChild(textContent);
-
-    // add the newly created element and its content into the DOM
-    const entriesList = document.getElementById("entries-holder");
-    const currentDiv = document.getElementById("end-block");
-    entriesList.insertBefore(newDiv, currentDiv);
+    return newNote;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    //used Salman's example journal entries from their own branch.
-    //This code will need to be updated as we change the nature of the journal entries object.
-    const journalEntries = [
-      {
-          title : "Journal Entry Title 1",
-          date : "2021-05-25",
-          bodyText : "## This is my first journal entry",
-          priority : 5
-      },
-      {
-          title : "Journal Entry Title 2",
-          date : "2021-05-24",
-          bodyText : "## This is my second journal entry",
-          priority : 10
-      },
-      // Add more journal entries as needed...
-    ]
+/**
+ * TODO: make it load, display, and sort entries from local storage.
+ */
 
-    window.localStorage.setItem("journalEntries", JSON.stringify(journalEntries));
-    let newObject = window.localStorage.getItem("journalEntries");
-    let parsedObject = JSON.parse(newObject);
+// Run the init() function when the page has loaded
+window.addEventListener("DOMContentLoaded", init);
 
-    parsedObject.forEach((entry) => loadEntries(entry['title'] + ' ' + entry['date']));
+// Starts the program, all function calls trace back here
+function init() {
+  // Initialize the tables in local storage if they do not exist
+    const tables = ["NoteTable", "ProjectTable", "TaskTable"];
+    tables.forEach(table => {
+        if (window.localStorage.getItem(table) === null) {
+            localStorage.setItem(table, JSON.stringify({}));
+        }
+    })
 
-    const sortBox = document.querySelector("select");
-    const entriesList = document.getElementById("entries-holder");
-    
+  // Initialize the ID container to check whether an ID is used
+    if (window.localStorage.getItem("IDContainer") === null) {
+        localStorage.setItem("IDContainer", JSON.stringify([]));
+    }
+    const IDContainer = window.localStorage.getItem("IDContainer");
 
-    sortBox.addEventListener("input", function reSort(event){
-        //clear out the existing list.
-        let currEntries = entriesList.childNodes;
-        while(currEntries.length > 2)
-        {
-            if(currEntries[0].id != 'end-block'){
-                entriesList.removeChild(currEntries[0]);
-            }
-        };
+    let loadedNotes = [];
 
-        //sort the list based on selected option.
-        if(event.target.value == 'date')
-            parsedObject.sort((a, b) => {
-            let da = new Date(a['date']),
-                db = new Date(b['date']);
-            return da - db;
-        });
+    //get and store all notes in an array
+    for(let i = 0; i < IDContainer.length; i++)
+    {
+        loadedNotes.push(loadEntry(getNoteFromTable(IDContainer[i])));
+    }
 
-        else if(event.target.value == 'title')
-        parsedObject.sort((a, b) => {
-            if(a['title'] > b['title'])
-                return 1;
-            else if(a['title'] < b['title'])
-                return -1;
-            return 0;
-        });
+    //init sort functions
+    const sortFunctions = newMap();
 
-        else if(event.target.value == 'priority')
-        parsedObject.sort((a, b) => {
-            let intA = Number(a['priority']);
-            let intB = Number(b['priority']);
-            return intB - intA;
-            //gets ascending order of priorities.
-        });
-
-        //reload the entries.
-        parsedObject.forEach((entry) => loadEntries(entry['title'] + ' ' + entry['date']));
+    sortFunctions.set('title', (noteA, noteB) => {
+        if(noteA['title'] > noteB['title'])
+            return 1;
+        else if(noteA['title'] < noteB['title'])
+            return -1;
+        return 0;
     });
-});
+
+    sortFunctions.set('date', (noteA, noteB) => {
+        let da = new Date(noteA['date']),
+            db = new Date(noteB['date']);
+        return da - db;
+    });
+
+    sortFunctions.set('lastEdited', (noteA, noteB) => {
+        let da = new Date(noteA['lastEdited']),
+            db = new Date(noteB['lastEdited']);
+        return da - db;
+    })
+    //sort notes by title to start
+    //NOTE: would not be opposed to saving the last sort-type used in local storage so we can use that.
+    loadedNotes.sort(sortFunctions['title']);
+    const sortBox = document.querySelector("select");
+
+    //display notes
+    //NOTE: would like to add pagination
+    const entriesHolder = document.querySelector("#entries-holder");
+    const endBlock = document.querySelector("#end-block");
+    function displayNotes()
+    {
+        //clear current display 
+        const children = entriesHolder.children;
+        while(children[0] != endBlock)
+            entriesHolder.removeChild(children[0]);
+
+        //put all the notes in
+        for(let i = 0; i < loadedNotes.length; i++)
+        {
+            entriesHolder.insertBefore(loadedNotes[i], endBlock);
+        }
+    }
+    sortBox.addEventListener("input", function sortAndDisplay(event)
+    {
+        loadedNotes.sort(sortFunctions[event.target.value]);
+        displayNotes();
+    });
+}
