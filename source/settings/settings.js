b17579693da541713508239c1aaedac84d7cd19b
sarahpaulll <spaull@ucsd.edu>
2024-05-30 11:38:24 -0700

    add functionality to back button

--- source/settings/settings.js
+++ source/settings/settings.js
@@ -1,6 +1,9 @@
 // Load Page
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const backButton = document.getElementById("back-button");
+  backButton.addEventListener("click", () => {
+    window.location.href = '../homepage/index.html';
+  });
 });
 
 
