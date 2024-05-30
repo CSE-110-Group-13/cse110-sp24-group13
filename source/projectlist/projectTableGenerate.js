b372f046b5ae821bdb87bb2e88a46a731029f355
addyng <110417388+addyng@users.noreply.github.com>
2024-05-30 14:40:38 -0700

    edited projectTableGenerate

--- source/projectlist/projectTableGenerate.js
+++ source/projectlist/projectTableGenerate.js
@@ -16,10 +16,29 @@ import {
   removeCompletedTaskFromProject
 } from "../backend/ProjectTable.js"
 
+import {
+  getNoteTableFromStorage,
+  saveNoteTableToStorage,
+  getNoteFromTable, 
+  saveNoteToTable, 
+  deleteNoteFromTable, 
+  createNewNoteObject, 
+  modifyNoteText, 
+  modifyNoteDate, 
+  modifyNoteLastEdited, 
+  modifyNoteTitle, 
+  appendProjectToNoteProjectList, 
+  removeProjectFromNoteProjectList, 
+  modifyNoteFavorited, 
+  appendTagToNoteTags, 
+  removeTagFromNoteTags
+} from "../backend/NoteTable.js"
+
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   const projectTable = getProjectTableFromStorage();
+  const noteTable = getNoteTableFromStorage();
   if (Object.keys(projectTable).length < 6) {
     createNewProjectObject("Project1", "Description1", ['task1', 'task2', 'task3'], "2024-05-30", "Priority1");
     createNewProjectObject("Project2", "Description2", ['task1', 'task2', 'task3'], "2024-05-30", "Priority2");
@@ -27,5 +46,13 @@ function init() {
     createNewProjectObject("Project3", "Description3", ['task1', 'task2', 'task3'], "2024-05-30", "Priority3");
     createNewProjectObject("Project3", "Description3", ['task1', 'task2', 'task3'], "2024-05-30", "Priority3");
     createNewProjectObject("Project3", "Description3", ['task1', 'task2', 'task3'], "2024-05-30", "Priority3");
+  }
+
+  if (Object.keys(noteTable).length < 5) {
+    createNewNoteObject("this is text1", "2024-05-29", "2024-05-30", "this is title1", ["project1", "project2"], true, ["tag1", "tag2", "tag3"]);
+    createNewNoteObject("this is text2", "2024-05-29", "2024-05-30", "this is title2", ["project1", "project2"], false, ["tag1", "tag2", "tag3"]);
+    createNewNoteObject("this is text3", "2024-05-29", "2024-05-30", "this is title3", ["project1", "project2"], true, ["tag1", "tag2", "tag3"]);
+    createNewNoteObject("this is text4", "2024-05-29", "2024-05-30", "this is title4", ["project1", "project2"], false, ["tag1", "tag2", "tag3"]);
+    createNewNoteObject("this is text5", "2024-05-29", "2024-05-30", "this is title5", ["project1", "project2"], true, ["tag1", "tag2", "tag3"]);
   }
 }
