b9c1f195e9fdcb5da79346e33951e078050a3107
Uliyaah Dionisio <uliyaah@gmail.com>
2024-05-31 12:12:59 -0700

    Set up intial js files for edit and view project pages

---
+++ source/project/editProject.js
@@ -0,0 +1,14 @@
+import {
+    projectID,
+    title,
+    description,
+    taskList,
+    deadline, 
+    priority,
+    dateCreated,
+    tasksCompleted
+    } from '../backend/ProjectTable.js';
+
+    let PROJECT_ID = "";
+
+    window.addEventListener("DOMContentLoaded", init);
---
+++ source/project/viewProject.js
@@ -0,0 +1,7 @@
+import { 
+    getProjectFromTable
+} from '../backend/ProjectTable.js';
+
+let PROJECT_ID = "";
+
+window.addEventListener("DOMContentLoaded", init);
