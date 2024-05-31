3c7d06fba9b750115b4a9f974bfc034e99c0dfd1
Uliyaah Dionisio <uliyaah@gmail.com>
2024-05-31 12:57:00 -0700

    Should now save project title, deadline, and decription to local storage

--- source/project/editProject.js
+++ source/project/editProject.js
@@ -1,14 +1,84 @@
+// editProject.js
+
 import {
-    projectID,
-    title,
-    description,
-    taskList,
-    deadline, 
-    priority,
-    dateCreated,
-    tasksCompleted
-    } from '../backend/ProjectTable.js';
+    createNewProjectObject, 
+    modifyProjectText, 
+    modifyProjectDate, 
+    modifyProjectTitle, 
+    getProjectFromTable
+} from '../backend/ProjectTable.js';
 
-    let PROJECT_ID = "";
+let PROJECT_ID = "";
 
-    window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("DOMContentLoaded", init);
+
+    
+/**
+ * Initializes the project editing process.
+ * If a project ID is not set, creates a new project object.
+ * If a project ID is set, populates the project with existing data.
+ * Attaches an event listener to the save button.
+ */
+function init() {
+    
+    PROJECT_ID = window.location.hash.substring(1);
+    if (!PROJECT_ID) {
+        const newProject = createNewProjectObject();
+        PROJECT_ID = newProject.projectID;
+    } else {
+        populateProject();
+    }
+    attachSaveButtonListener();
+    attachCancelButtonListener();
+}
+
+/**
+ * Attaches an event listener to the save button.
+ */
+function attachSaveButtonListener() {
+    document.querySelector('save-button button').addEventListener('click', saveProject);
+}
+
+function attachCancelButtonListener() {
+    document.querySelector('cancel-button button').addEventListener('click', cancelEdit);
+}
+
+
+/**
+ * Saves the current state of the project to the local storage.
+ * Retrieves the project title, markdown content, and date from the DOM.
+ * Modifies the project in the backend using the project ID.
+ */
+function saveProject() {
+    const projectTitle = document.querySelector('.projectTitle h1').innerText;
+    const projectDescription = document.querySelector('markdown-editor').wysimark.getMarkdown();
+    const projectDeadline = document.querySelector('.date input').value;
+    // Modify the project
+    modifyProjectTitle(PROJECT_ID, projectTitle);
+    modifyProjectText(PROJECT_ID, projectDescription);
+    modifyProjectDate(PROJECT_ID, projectDeadline);
+    window.location.href = './view-project.html#' + PROJECT_ID;
+    
+}
+
+function cancelEdit() {
+    if(!PROJECT_ID){
+        window.location.href = "../projectlist/projectlist.html";
+    }
+    else{
+        window.location.href = './view-project.html#' + PROJECT_ID;
+    }
+    
+}
+/**
+ * Populates the project with existing data from the backend.
+ * Retrieves the project data using the project ID.
+ * Sets the project title, deadline, and date in the DOM.
+ */
+function populateProject() {
+    const project = getProjectFromTable(PROJECT_ID);
+    console.log(project.title)
+    document.querySelector('#projectTitle h1').innerText = project.title;
+    document.querySelector('#deadline').value = project.deadline;
+    document.querySelector('#projectDesc').innerText = project.description;
+}
