7c1a9b0bfe9eb6ba62ad460ba6a0b8a888d505a7
Hoang <21hoangphan@gmail.com>
2024-06-02 17:32:48 -0700

    update test boilerplates

--- source/__tests__/endToEnd.test.js
+++ source/__tests__/endToEnd.test.js
@@ -9,7 +9,7 @@ describe('End to end tests of the app', () => {
 
   // Before all tests, launch a browser and open a new page
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: false }); // Set to true if you don't want to see the browser UI
+    browser = await puppeteer.launch({ headless: true }); // Set to true if you don't want to see the browser UI
     page = await browser.newPage();
     await page.goto(URL + '/homepage/index.html');
 
@@ -27,6 +27,6 @@ describe('End to end tests of the app', () => {
   });
 
   it('Sample test', async () => {
-    // expect(1 + 1).toBe(2);
+    expect(1).toBe(1);
   });
 });
