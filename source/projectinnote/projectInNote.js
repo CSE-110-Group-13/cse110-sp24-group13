dbac80f3a9d114aa0e21a9d98747bf5ecda3e2d6
sarahpaulll <spaull@ucsd.edu>
2024-06-04 13:28:44 -0700

    fix styling and add todo comments

--- source/projectinnote/projectInNote.js
+++ source/projectinnote/projectInNote.js
@@ -56,6 +56,15 @@ class linkedProject extends HTMLElement {
         projectDesc.innerHTML = `${project.description}`;
         projectProgress.innerHTML = `${project.progress}`;
 
+        //TODO: Add tasks to the task container
+        let taskContainer = document.getElementsByClassName('tasks');
+
+        //TODO: Add hrefs to projectView and projectChange
+        let projectViewLink = document.querySelector('.projectHeader a');
+        let projectChange = document.querySelector('.projectFooter a');
+
+
+
     }
 
     render() {
@@ -229,6 +238,17 @@ class linkedProject extends HTMLElement {
             margin-right: 15px;
         }
 
+        .projectHeader a {
+            margin-left: auto;
+            margin-right: 10px;
+            cursor: pointer;
+        }
+
+        .projectHeader .progress {
+            margin-left: 20px;
+            margin-right: auto;
+        }
+
         .projectDue {
             display: flex;
             flex-direction: row;
@@ -283,6 +303,32 @@ class linkedProject extends HTMLElement {
 
         #projectDescContent.close {
             display: none;
+        }
+
+        .projectFooter {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+
+        .projectFooter a {
+            display: flex;
+            justify-content: center; 
+            align-items: center; 
+            color: #8B0000;
+            font-weight: bold;
+            font-size: 12px; 
+            margin-left: auto;
+            margin-right: 10px;
+            cursor: pointer;
+            
+        }
+
+        .projectFooter svg {
+            fill: #8B0000;
+            margin-left: 4px; /* Add space between icon and text */
+            width: 15px;
+            height: 15px;
         }
 
         .description {
@@ -367,6 +413,7 @@ class linkedProject extends HTMLElement {
                     <progress id="progressBar" value="33" max="100">200000</progress>
                     <label for="progressBar">placeholder%</label>
                 </div>
+                <a id=projectView><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg></a>
             </div>
             <div class="projectDetails">
                 <div class="projectDue">
@@ -389,15 +436,17 @@ class linkedProject extends HTMLElement {
                 <p id="projectDescContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus beatae eum perferendis accusantium quasi odio vel voluptatem temporibus nihil sed reprehenderit quo eveniet delectus reiciendis dolor itaque, nemo quas dolore!</p>
                 <div class="projectTaskList">
                     <h3>Tasks</h3>
-                    <div class="tasks">
-                    </div>   
+                    <ul class="tasks"></ul>   
                     <button id="addTasks">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
                         <i>Add new task</i>
                     </button>
-                    <p><i>This is the project linked to this note, check off tasks that you have completed for this note</i></p>
                 </div>
             </div>
+            <footer class="projectFooter">
+                <p><i>This is the project linked to this note, check off tasks that you have completed for this note</i></p>
+                <a id="changeProject">Change Project <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a>
+            </footer>
         </div>`;
         container.appendChild(linkedProjectComponent);
         container.appendChild(linkAProject);
@@ -479,12 +528,12 @@ class linkedProject extends HTMLElement {
         });
 
 
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', () => {
             const descDropdownButton = document.getElementById('descDropdown');
             const projectDescContent = document.getElementById('projectDescContent');
         
             // Add event listener for when the descDropdown button is clicked
-            descDropdownButton.addEventListener('click', function() {
+            descDropdownButton.addEventListener('click', () => {
                 projectDescContent.classList.toggle('close');
 
                 if(projectDescContent.classList.contains('close')) {
@@ -493,6 +542,15 @@ class linkedProject extends HTMLElement {
                 else {
                     descDropdownButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>'
                 }
+            });
+
+            // TODO Add new tasks
+            const taskContainer = document.getElementsByClassName('tasks');
+            const addTasks = document.getElementById("addTasks");
+
+            //TODO Add event listener for when add tasks is pressed
+            addTasks.addEventListener('click', () => {
+        
             });
         });
         
