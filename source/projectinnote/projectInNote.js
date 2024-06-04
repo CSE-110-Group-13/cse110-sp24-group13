4cf792f7b95b1d0dc2c8970c6840f709323c7968
sarahpaulll <spaull@ucsd.edu>
2024-06-03 21:12:42 -0700

    add svg for add new task button

--- source/projectinnote/projectInNote.js
+++ source/projectinnote/projectInNote.js
@@ -238,11 +238,40 @@ class linkedProject extends HTMLElement {
             margin-right: 5px;
         }
         
+        .projectTaskList {
+            display: flex;
+            flex-direction: column; 
+        }
+
+        #addTasks {
+            border: none;
+            background-color: transparent;
+            outline: none;
+            box-shadow: none;
+            cursor: pointer;
+            width: 100%;
+            justify-content: start;
+        }
+
+        .projectTaskList p {
+            color: #7C7C7C;
+        }
+
+        #addTasks svg {
+            margin-right: 8px; /* Add space between icon and text */
+            width: 20px;
+            height: 20px;
+        }
+
+
+        
         .description {
             width: 60vw;
             background-color: #F8F8F8;
             border-radius: 25px; 
             padding: 5px 20px;
+            margin-left: auto;
+            margin-right: 3vw;
         }
         
         .description p {
@@ -338,8 +367,11 @@ class linkedProject extends HTMLElement {
                     <h3>Tasks</h3>
                     <div class="tasks">
                     </div>   
-                    <button id="addTasks">Add Tasks</button>
-                    <p>This is the project linked to this note, check off tasks that you have completed for this note</p>
+                    <button id="addTasks">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                        <i>Add new task</i>
+                    </button>
+                    <p><i>This is the project linked to this note, check off tasks that you have completed for this note</i></p>
                 </div>
             </div>
         </div>`;
