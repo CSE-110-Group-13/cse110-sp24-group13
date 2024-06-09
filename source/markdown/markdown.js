b9877236c87db84c7711de5887546b84e1038a3b
addyng <110417388+addyng@users.noreply.github.com>
2024-06-09 13:08:03 -0700

    cleaned up and commented notes folder

--- source/markdown/markdown.js
+++ source/markdown/markdown.js
@@ -1,20 +1,20 @@
 class markdownEditor extends HTMLElement {
   /**
- * Creates an instance of markdownEditor.
- *
- * @constructor
- */
-constructor() {
+   * Creates an instance of markdownEditor.
+   *
+   * @constructor
+   */
+  constructor() {
     super();
     this.wysimark = null;
   }
 
   /**
- * The real constructor. Makes all the sub elemenrs and appends them to the main element.
- * It also initializes the wysimark editor.
- * It also adds event listeners to the save and load buttons.
- */
-connectedCallback() {
+   * The real constructor. Makes all the sub elemenrs and appends them to the main element.
+   * It also initializes the wysimark editor.
+   * It also adds event listeners to the save and load buttons.
+   */
+  connectedCallback() {
     // Create a div element
     const container = document.createElement('div');
     const editor = document.createElement('div');
@@ -28,51 +28,22 @@ connectedCallback() {
 
     // Append elements to the container
     container.appendChild(editor);
-    /*
-    container.appendChild(saveButton);
-    container.appendChild(loadButton);
-    */
     // Append the container to the element itself
     this.appendChild(container);
 
     const foundEditor = document.getElementById('editor');
     // Initialize wysimark
     this.wysimark = createWysimark(foundEditor, { placeholder: 
-    "Hello World \n" +
-    "lorem ipsum dolor sit amet. Goo goo g'joob\n" +
-    "more example text\n" +
-    "filling this up"
+      "Hello World \n" +
+      "lorem ipsum dolor sit amet. Goo goo g'joob\n" +
+      "more example text\n" +
+      "filling this up"
     });
 
     // Add event listeners
     saveButton.addEventListener('click', () => this.save());
     loadButton.addEventListener('click', () => this.load());
   }
-
-
-  
-/*
- * Uses the built in wysimark method to save the markdown content to local storage.
-
-save() {
-    if (this.wysimark) {
-      const markdown = this.wysimark.getMarkdown();
-      window.localStorage.setItem("markdown", markdown.toString());
-    }
-  }
-  
-  
-  /**
- * Uses built im wysimark method to load the markdown content from local storage.
- 
-load() {
-    const savedMarkdownContent = window.localStorage.getItem("markdown");
-    if (this.wysimark && savedMarkdownContent) {
-      this.wysimark.setMarkdown(savedMarkdownContent);
-      console.log(savedMarkdownContent);
-    }
-  }  
-  */
 }
 
 customElements.define('markdown-editor', markdownEditor);
--- source/note/editNote.js
+++ source/note/editNote.js
@@ -1,20 +1,18 @@
 // editNote.js
 import { 
-    createNewNoteObject, 
-    modifyNoteText, 
-    modifyNoteDate, 
-    modifyNoteTitle, 
-    appendTagToNoteTags, 
-    removeTagFromNoteTags,
-    getNoteFromTable,
-    modifyNoteLastEdited,
-  } from '../backend/NoteTable.js';
+	createNewNoteObject, 
+	modifyNoteText, 
+	modifyNoteDate, 
+	modifyNoteTitle, 
+	appendTagToNoteTags, 
+	removeTagFromNoteTags,
+	getNoteFromTable,
+	modifyNoteLastEdited,
+} from '../backend/NoteTable.js';
 
 import {
-    getProjectFromTable
+	getProjectFromTable
 } from '../backend/ProjectTable.js';
-
-
 
 let NOTE_ID = "";
 
@@ -27,35 +25,33 @@ window.addEventListener("DOMContentLoaded", init);
  * Attaches an event listener to the save button.
  */
 function init() {
-    
-    NOTE_ID = window.location.hash.substring(1);
-    if (!NOTE_ID) {
-        //Uncomment these 2 lines if you're starting live-server from this page
-        //localStorage.setItem("NoteTable", JSON.stringify({}));
-        //localStorage.setItem("IDContainer", JSON.stringify([]));
-        const newNote = createNewNoteObject("Default Text", new Date().toISOString().slice(0,10), new Date().toISOString().slice(0,10), "Default Title", "", "", []);
-        NOTE_ID = newNote.noteID;
-        window.location.href = './edit-note.html#' + NOTE_ID;
-    } else {
-        populateNote();
-        populateTag();
-        populateProjectTag();
-    }
-    attachSaveButtonListener();
-    attachCancelButtonListener();
+	NOTE_ID = window.location.hash.substring(1);
+	if (!NOTE_ID) {
+		const newNote = createNewNoteObject("Default Text", new Date().toISOString().slice(0,10), new Date().toISOString().slice(0,10), "Default Title", "", "", []);
+		NOTE_ID = newNote.noteID;
+		window.location.href = './edit-note.html#' + NOTE_ID;
+	} else {
+		populateNote();
+		populateTag();
+		populateProjectTag();
+	}
+	attachSaveButtonListener();
+	attachCancelButtonListener();
 }
 
 /**
  * Attaches an event listener to the save button.
  */
 function attachSaveButtonListener() {
-    document.querySelector('save-button button').addEventListener('click', saveNote);
+	document.querySelector('save-button button').addEventListener('click', saveNote);
 }
 
+/**
+ * Attaches an event listener to the cancel button.
+ */
 function attachCancelButtonListener() {
-    document.querySelector('cancel-button button').addEventListener('click', cancelEdit);
+	document.querySelector('cancel-button button').addEventListener('click', cancelEdit);
 }
-
 
 /**
  * Saves the current state of the note to the local storage.
@@ -63,112 +59,119 @@ function attachCancelButtonListener() {
  * Modifies the note in the backend using the note ID.
  */
 function saveNote() {
-    const noteTitle = document.querySelector('.noteTitle input').value;
-    const noteMarkdown = document.querySelector('markdown-editor').wysimark.getMarkdown();
-    const noteDate = document.querySelector('.date input').value;
-    const tagElements = document.querySelectorAll('.tagContainer li');
-    // Modify the note
-    modifyNoteTitle(NOTE_ID, noteTitle);
-    modifyNoteText(NOTE_ID, noteMarkdown);
-    modifyNoteDate(NOTE_ID, noteDate);
-    modifyNoteLastEdited(NOTE_ID, new Date().toISOString().slice(0,10));
+	const noteTitle = document.querySelector('.noteTitle input').value;
+	const noteMarkdown = document.querySelector('markdown-editor').wysimark.getMarkdown();
+	const noteDate = document.querySelector('.date input').value;
+	const tagElements = document.querySelectorAll('.tagContainer li');
+	// Modify the note
+	modifyNoteTitle(NOTE_ID, noteTitle);
+	modifyNoteText(NOTE_ID, noteMarkdown);
+	modifyNoteDate(NOTE_ID, noteDate);
+	modifyNoteLastEdited(NOTE_ID, new Date().toISOString().slice(0,10));
 
-    const note = getNoteFromTable(NOTE_ID);
-    const currentTags = note.tags;
-    if (currentTags) {
-        currentTags.forEach(tag => {
-            removeTagFromNoteTags(NOTE_ID, tag);
-        });
-    }
-    for(let i = 0; i < tagElements.length; i++) {
-        const tag = tagElements[i].textContent;
-        appendTagToNoteTags(NOTE_ID, tag);
-    }
+	const note = getNoteFromTable(NOTE_ID);
+	const currentTags = note.tags;
+	if (currentTags) {
+		currentTags.forEach(tag => {
+			removeTagFromNoteTags(NOTE_ID, tag);
+		});
+	}
+	for(let i = 0; i < tagElements.length; i++) {
+		const tag = tagElements[i].textContent;
+		appendTagToNoteTags(NOTE_ID, tag);
+	}
 
-    window.location.href = './view-note.html#' + NOTE_ID;
+	window.location.href = './view-note.html#' + NOTE_ID;
 }
 
+/**
+ * On canceling edit, go back to homepage
+ */
 function cancelEdit() {
     window.location.href = "../homepage/index.html";
 }
+
 /**
  * Populates the note with existing data from the backend.
  * Retrieves the note data using the note ID.
  * Sets the note title, markdown content, and date in the DOM.
  */
 function populateNote() {
-    const note = getNoteFromTable(NOTE_ID);
-    document.querySelector('.noteTitle input').value = note.title;
-    document.querySelector('markdown-editor').wysimark.setMarkdown(note.text);
-    document.querySelector('.date input').value = note.date;
+	const note = getNoteFromTable(NOTE_ID);
+	document.querySelector('.noteTitle input').value = note.title;
+	document.querySelector('markdown-editor').wysimark.setMarkdown(note.text);
+	document.querySelector('.date input').value = note.date;
 }
 
+/**
+ * Add tag to note
+ */
 function populateTag() {
-    const note = getNoteFromTable(NOTE_ID);
-    const tags = note.tags;
-    const tagsContainer = document.querySelector('.tagContainer');
-    tags.forEach(tag => {
-        const newTag = document.createElement('li');
-        newTag.textContent = tag;
-        // Deleting tag
-        let isClickedOnce = false;
-        newTag.addEventListener('click', () => {
-            if (!isClickedOnce) {
-                newTag.style.backgroundColor = "#FF000F";
-                setTimeout(() => {
-                    newTag.style.backgroundColor = "";
-                    isClickedOnce = false;
-                }, 2000);
-                isClickedOnce = true;
-            }
-            else {  
-                newTag.remove();
-            }
-        });
-        tagsContainer.appendChild(newTag);
-    });
+	const note = getNoteFromTable(NOTE_ID);
+	const tags = note.tags;
+	const tagsContainer = document.querySelector('.tagContainer');
+	tags.forEach(tag => {
+		const newTag = document.createElement('li');
+		newTag.textContent = tag;
+		// Deleting tag
+		let isClickedOnce = false;
+		newTag.addEventListener('click', () => {
+			if (!isClickedOnce) {
+				newTag.style.backgroundColor = "#FF000F";
+				setTimeout(() => {
+					newTag.style.backgroundColor = "";
+					isClickedOnce = false;
+				}, 2000);
+				isClickedOnce = true;
+			}
+			else {  
+				newTag.remove();
+			}
+		});
+		tagsContainer.appendChild(newTag);
+	});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const newTagForm = document.getElementById("newTag");
-    const newTagInput = document.getElementById("newTagInput");
-    newTagForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-    });
-    // Add tag
-    document.addEventListener('click', (event) => {
-        const isClickInside = newTagForm.contains(event.target);
+	const newTagForm = document.getElementById("newTag");
+	const newTagInput = document.getElementById("newTagInput");
+	newTagForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+	});
+	// Add tag
+	document.addEventListener('click', (event) => {
+		const isClickInside = newTagForm.contains(event.target);
 
-        if(!isClickInside) {
-            const tag = newTagInput.value.trim();
-            if (tag) {
-                newTagInput.value = '';
-                const tagsContainer = document.querySelector('.tagContainer');
-                const newTag = document.createElement('li');
-                newTag.textContent = tag;
-                // Deleting tag
-                let isClickedOnce = false;
-                newTag.addEventListener('click', () => {
-                if (!isClickedOnce) {
-                    newTag.style.backgroundColor = "#FF000F";
-                    setTimeout(() => {
-                        newTag.style.backgroundColor = "";
-                        isClickedOnce = false;
-                    }, 2000);
-                    isClickedOnce = true;
-                }
-            else {  
-                newTag.remove();
-            }
+		if(!isClickInside) {
+			const tag = newTagInput.value.trim();
+			if (tag) {
+				newTagInput.value = '';
+				const tagsContainer = document.querySelector('.tagContainer');
+				const newTag = document.createElement('li');
+				newTag.textContent = tag;
+				// Deleting tag
+				let isClickedOnce = false;
+				newTag.addEventListener('click', () => {
+					if (!isClickedOnce) {
+						newTag.style.backgroundColor = "#FF000F";
+						setTimeout(() => {
+							newTag.style.backgroundColor = "";
+							isClickedOnce = false;
+						}, 2000);
+						isClickedOnce = true;
+					}
+					else {  
+							newTag.remove();
+					}
         });
-                tagsContainer.appendChild(newTag);
-            }
-        }
-    });
+				tagsContainer.appendChild(newTag);
+			}	
+		}
+	});
 
-    document.addEventListener('projectChanged', () => {
-        populateProjectTag();
-    });
+	document.addEventListener('projectChanged', () => {
+		populateProjectTag();
+	});
 });
 
 /**
@@ -177,38 +180,37 @@ document.addEventListener('DOMContentLoaded', () => {
  * Retrieves the project data using the project ID from linkedProject
  */
  function populateProjectTag() {
-    const linkProjectElement = document.getElementById("linkedProjectTag");
-    const projectTagContainer = document.querySelector(".projectTagContainer");
-    const note = getNoteFromTable(NOTE_ID);
-    if(note.linkedProject === "") {
-        projectTagContainer.classList.add("close");
-        if(projectTagContainer.classList.contains("open")) {
-            projectTagContainer.classList.remove("open");
-        }
-    }
-    else {
-        const project = getProjectFromTable(note.linkedProject);
-        linkProjectElement.textContent = project.title;
-        linkProjectElement.href = "../project/view-project.html" + "#" + note.linkedProject;
-        // get priority
-        const priority = document.getElementById("priority");
-        if (project.priority === "high") {
-            priority.style.backgroundColor = "#FF000F";
-        }
-        else if (project.priority === "medium") {
-            priority.style.backgroundColor = "#FFD600";
-        }
-        else if (project.priority === "low") {
-        priority.style.backgroundColor = "#0AB73B"
-        }
-        if (projectTagContainer.classList.contains("close")){
-            projectTagContainer.classList.remove("close");
-        }
-        projectTagContainer.classList.add("open");
-    }
+	const linkProjectElement = document.getElementById("linkedProjectTag");
+	const projectTagContainer = document.querySelector(".projectTagContainer");
+	const note = getNoteFromTable(NOTE_ID);
+	if(note.linkedProject === "") {
+		projectTagContainer.classList.add("close");
+		if(projectTagContainer.classList.contains("open")) {
+			projectTagContainer.classList.remove("open");
+		}
+	}
+	else {
+		const project = getProjectFromTable(note.linkedProject);
+		linkProjectElement.textContent = project.title;
+		linkProjectElement.href = "../project/view-project.html" + "#" + note.linkedProject;
+		// get priority
+		const priority = document.getElementById("priority");
+		if (project.priority === "high") {
+			priority.style.backgroundColor = "#FF000F";
+		}
+		else if (project.priority === "medium") {
+			priority.style.backgroundColor = "#FFD600";
+		}
+		else if (project.priority === "low") {
+			priority.style.backgroundColor = "#0AB73B"
+		}
+		if (projectTagContainer.classList.contains("close")){
+			projectTagContainer.classList.remove("close");
+		}
+		projectTagContainer.classList.add("open");
+	}
 }
 
 export {
     populateTag
 }
-
